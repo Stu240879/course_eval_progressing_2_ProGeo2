055f43daa055be50c1f224f3d9a61a5d28867c90
Kilian Etter <kilianetter@web.de>
2022-12-12 18:03:19 +0100

    wip

--- load.py
+++ load.py
@@ -1,20 +1,45 @@
 
 import pandas as pd
 
+def originalData():
+    import os
+    dirname = os.path.dirname(__file__)
+    lp = os.path.join(dirname, 'data\original\Eval ProGeo.xlsx')
+    lek1 = os.path.join(dirname, 'data\original\Eval L1.xlsx')
+    lek2 = os.path.join(dirname, 'data\original\Eval L2.xlsx')
+    lek3 = os.path.join(dirname, 'data\original\Eval L3.xlsx')
+    lek4 = os.path.join(dirname, 'data\original\Eval L4.xlsx')
+    lek5 = os.path.join(dirname, 'data\original\Eval L5.xlsx')
+    lek6 = os.path.join(dirname, 'data\original\Eval L6.xlsx')
+    lek7 = os.path.join(dirname, 'data\original\Eval L7.xlsx')
+    fps = [lp, lek1, lek2, lek3, lek4, lek5, lek6, lek7]
+    return fps
+
 def sampleData():
     import os
     dirname = os.path.dirname(__file__)
-    course = os.path.join(dirname, 'data\Eval ProGeo.xlsx')
+    lp = os.path.join(dirname, 'data\Eval ProGeo.xlsx')
     lek1 = os.path.join(dirname, 'data\Eval L1.xlsx')
     lek2 = os.path.join(dirname, 'data\Eval L2.xlsx')
     lek3 = os.path.join(dirname, 'data\Eval L3.xlsx')
     lek4 = os.path.join(dirname, 'data\Eval L4.xlsx')
     lek5 = os.path.join(dirname, 'data\Eval L5.xlsx')
     lek6 = os.path.join(dirname, 'data\Eval L6.xlsx')
-    lek7 = os.path.join(dirname, 'data\Eval L7.xlsx') 
-    fps = [course, lek1, lek2, lek3, lek4, lek5, lek6, lek7]
- 
+    lek7 = os.path.join(dirname, 'data\Eval L7.xlsx')
+    fps = [lp, lek1, lek2, lek3, lek4, lek5, lek6, lek7]
     return fps
+
+def samplePath():
+    import os
+    dirname = os.path.dirname(__file__)
+    path = os.path.join(dirname, 'data')
+    return path
+
+def outpath(fname):
+    import os
+    path = samplePath()
+    outpath = os.path.join(path, f'{fname}.xlsx')
+    return outpath
 
 
 def loadData(file = "", nrows=0):
--- rectify.py
+++ rectify.py
@@ -4,7 +4,85 @@ import matplotlib.pyplot as plt
 from textwrap import wrap
 import re
 
-from load import loadData, getQuestions, sampleData
+from load import loadData, getQuestions, originalData, sampleData, outpath
+
+
+def rect_LPeval(filepath):
+    # load data
+    evaldf = pd.read_excel(filepath)
+    evaldf_head = evaldf.columns
+    headerList = list(evaldf_head)
+    names = headerList
+    
+    # init
+    i = 0
+    header = []
+    questions = []
+
+    # move col_name right
+    rMove = [11,50,57,59,67,69,71,74,77]
+    for ncol in rMove:
+        names[ncol] = names[ncol-1]
+        names[ncol-1] = "empty"
+
+
+    # move col_name left
+    lMove = [12,22]
+    for ncol in lMove:
+        names[ncol] = names[ncol+1]
+        names[ncol+1] = "empty"
+
+    # delete columns
+    delete_cols = [2,3,4,5,6]
+    
+    # split questions from numbers and store in separate lists
+    for n in names:
+        i+=1
+        if re.match('\A([0-9|A-Z].[0-9])', n) == None: 
+            num = None
+            qu = None
+        elif re.match('\A([A-Z].[0-9])', n) != None:
+            strlist = re.split('\A([A-Z].[0-9])', n)
+            num = strlist[1]
+            qu = strlist[2].lstrip()
+        elif re.match('\A([0-9].[0-9].[0-9])', n) != None:
+
+            strlist = re.split('\A([0-9].[0-9].[0-9])', n)
+            num = strlist[1]
+            qu = strlist[2].lstrip()
+        elif re.match('\A([0-9].[0-9])', n) != None:
+            strlist = re.split('\A([0-9].[0-9])', n)
+            num = strlist[1]
+            qu = strlist[2].lstrip()
+        else:
+            print(f'you done fucked up at col {i}')
+    
+        header.append(num)
+        questions.append(qu)
+    
+    # make new table header from qid
+    header[0] = "A.0 Nutzer"
+    header[1] = "A.0 Abgabedatum"
+
+    evaldf.columns = header
+
+    ### check if whole col is na and delete
+    ### !!! This will delete all columns where there is no data....even if you want it to be NA !!!
+    evaldf = evaldf.dropna(axis=1,how='all')
+
+
+    # construct questions dataframe
+    qdf = pd.DataFrame({"qid": header, "num": None, "type": None, "frage": questions})
+    qdf = qdf[qdf["qid"].notnull()].reset_index()
+
+    # this will overwrite existing files!!!
+    fn_out = sampleData()[0]
+    evaldf.to_excel(fn_out, sheet_name="Eval", index=False)
+    with pd.ExcelWriter(fn_out, engine='openpyxl', mode='a') as writer:  
+        qdf.to_excel(writer, sheet_name='Fragen')
+    print("Excel file created")
+
+
 
 
 def rectify(filepath):
@@ -53,7 +131,7 @@ def rectify(filepath):
 
 
 
-def rectify_lessons(all=True, num:int=None):
+def rectify_LESeval(all=True, num:int=None):
     if all == True:
         fps = sampleData()
         fps.pop(0)
@@ -67,5 +145,8 @@ def rectify_lessons(all=True, num:int=None):
 
 if __name__ == "__main__":
     # pd.options.display.max_colwidth = 255
-
-    rectify_lessons(all=True)
+    # rectify_LESeval(all=True)
+    
+    fps = originalData()
+    fp_in = fps[0]
+    rect_LPeval(fp_in)
